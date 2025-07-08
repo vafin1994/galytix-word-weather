d584c15234f58e7b0f7c0cea14c3a19740b3340a
Oleksandr Vafin <vafinalex@gmail.com>
2025-07-08 21:44:53 +0200

    Add changes of units of measure Styling of weather page

--- src/app/api-services/fetch-weather.ts
+++ src/app/api-services/fetch-weather.ts
@@ -12,10 +12,11 @@ export class FetchWeather {
   constructor(private httpClient: HttpClient) {
   }
 
-  public fetchWeatherForCountry(countyName: string): Observable<WeatherData> {
+  public fetchWeatherForCountry(countyName: string, units: string = 'metric'): Observable<WeatherData> {
     const httpParams = new HttpParams()
       .set('q', countyName)
-      .set('APPID', FETCH_WEATHER_FOR_COUNTRY.APPID);
+      .set('APPID', FETCH_WEATHER_FOR_COUNTRY.APPID)
+      .set('units', units);
     return this.httpClient.get<WeatherData>(FETCH_WEATHER_FOR_COUNTRY.API, {params: httpParams});
   }
 }
--- src/app/weather-page/weather-page.ts
+++ src/app/weather-page/weather-page.ts
@@ -1,25 +1,51 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Data} from '@angular/router';
+import {ActivatedRoute, Data, Router} from '@angular/router';
 import {WeatherData} from '../models/weather.model';
 import {CommonModule} from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {FetchWeather} from '../api-services/fetch-weather';
+import {take} from 'rxjs';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-weather-page',
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatCardModule, MatButtonToggleModule],
   templateUrl: './weather-page.html',
   styleUrl: './weather-page.css'
 })
-export class WeatherPage implements OnInit{
+export class WeatherPage implements OnInit {
+
+  uomValue: 'metric' | 'imperial' = 'metric'
 
   weatherData: WeatherData | undefined;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private fetchWeather: FetchWeather) {
+  }
 
   ngOnInit() {
     this.activatedRoute.data.subscribe((data: Data) => {
       const weatherDate = data['weather'];
       this.weatherData = weatherDate ? weatherDate : undefined;
     })
+  }
+
+  onReturnButtonClicked() {
+    this.router.navigate(['/']);
+  }
+
+  changeUnitsOfMeasure(unitsOfMeasure: 'metric' | 'imperial') {
+    const country = this.activatedRoute.snapshot.params['country'];
+    this.fetchWeather.fetchWeatherForCountry(country, unitsOfMeasure).pipe(
+      take(1),
+    ).subscribe(
+      (response: WeatherData) => {
+        this.weatherData = response;
+      }
+    );
   }
 
 }
